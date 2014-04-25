2c7b82c26c432dd31b5e5252692f7892534a1986
Jia <lijia577@ubuntu.(none)>
2014-04-25 16:49:12 -0400

    jope its working

--- Test/Pancake/PancakeSourceCode/test.c
+++ Test/Pancake/PancakeSourceCode/test.c
@@ -2,7 +2,7 @@
 
 int main(){
 int i=0;
-int j= 0&&5;
+int j= 5&&0;
 
 printf("%d %d", i,j);
 return 0;
--- Test/e.sh
+++ Test/e.sh
@@ -1,3 +1,4 @@
 #!bin/bash
+clear
 gcc $1 -std=c99 -Werror -o $(basename $1 ".c")
 ./$(basename $1 ".c")
